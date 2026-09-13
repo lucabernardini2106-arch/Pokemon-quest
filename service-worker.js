c7f610d5bce4724930e559364f59d012ce68d8bf
lucabernardini2106-arch <lucabernardini2106@gmail.com>
2026-09-14 00:04:11 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-const C="pokemon-quest-v7-2";const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const C="pokemon-quest-v7-3";
+const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(u.pathname.endsWith(".js")||u.pathname.endsWith(".css")||u.pathname.endsWith("index.html")){
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+  } else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
