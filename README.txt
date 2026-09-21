6b98d83d799f3102e7606e78265b180a7fef8967
lucabernardini2106-arch <lucabernardini2106@gmail.com>
2026-09-21 20:08:12 +0200

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-Pokémon Quest v10.0
+Pokémon Quest v10.3
 
-Nuova versione con assegnazione Quest tramite checkbox, XP Group Quest a 30, obiettivi settimanali 350/800 XP con reset lunedì-domenica, barre di avanzamento, Medagliere, Pokédex e sistema di sblocco individuale dei nuovi Pokémon: a 4/8/12 Pokémon al Lv.30 ogni bambino sceglie 2 creature tra 5 alternative.
+Versione verificata e corretta: 10 Quest personali al giorno (5 fisse + 5 variabili), 90 XP personali/giorno, Quest di gruppo 30 XP, obiettivi settimanali 350/800 XP, bonus XP adulti, evoluzioni a Lv.10/Lv.20 e percorso individuale di sblocco: a 2/4/6/8/10/12 Pokémon al Lv.30 il bambino sceglie 1 Pokémon tra 3. Ogni bambino può arrivare a 18 Pokémon scoperti (12 iniziali + 6 scelti).
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const C="pokemon-quest-v10-1";
-const A=["./","./index.html","./styles.css?v=10.1","./app.js?v=10.1","./manifest.webmanifest"];
+const C="pokemon-quest-v10-3";
+const A=["./","./index.html","./styles.css?v=10.3","./app.js?v=10.3","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.pathname.endsWith(".js")||u.pathname.endsWith(".css")||u.pathname.endsWith("index.html")||u.pathname.endsWith("/")){e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))}else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
